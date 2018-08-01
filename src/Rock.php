451119c8df8fb73aaf3bce3ee9d26598970af60c
yixu <yixu@sureyee.com>
2018-08-01 09:13:56 +0800

    添加配置文件内容 完善接口请求流程

--- src/Rock.php
+++ src/Rock.php
@@ -9,21 +9,69 @@
 namespace Sureyee\LaravelRockFinTech;
 
 
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
+use Sureyee\RockFinTech\Client;
 use Sureyee\RockFinTech\Request;
 
+/**
+ * Class Rock
+ * @package Sureyee\LaravelRockFinTech
+ * @method
+ */
 class Rock
 {
 
+    const ACCOUNT_TYPE_BORROWER = 2;
+
+    protected $client;
+
     protected $request;
 
-    public function __construct(Request $request)
+    public function __construct(Client $client, Request $request)
     {
         $this->request = $request;
-        dd(__METHOD__);
+        $this->client = $client;
     }
 
-    public function createAccount()
+    public function createAccountP($mobile, $account_type, $role_type, $out_serial_no = null)
     {
+        $this->request->setService(snake_case(__FUNCTION__));
+        $params = [
+            'success_url' => Config::get('rock_fin_tech.success_url.create_account_p'),
+            'mobile' => $mobile,
+            'account_type' => $account_type,
+            'role_type' => $role_type,
+            'fail_url' => Config::get('rock_fin_tech.fail_url.create_account_p'),
+            'out_serial_no' => is_null($out_serial_no) ? $this->uniqueId() : $out_serial_no,
+        ];
 
+        $this->request->setParams($params);
+
+        return $this->send();
+
+    }
+
+    /**
+     * 发送请求
+     * @return \Sureyee\RockFinTech\Response|false
+     */
+    protected function send()
+    {
+        try {
+            return $this->client->request($this->request);
+        } catch (\Exception $exception) {
+            Log::error($exception->getMessage(), $this->request->getParams());
+        }
+        return false;
+    }
+
+    /**
+     * 生成32位唯一编号
+     * @return string
+     */
+    protected function uniqueId()
+    {
+        return md5(uniqid(md5(microtime(true)),true));
     }
 }
--- src/config/rock_fin_tech.php
+++ src/config/rock_fin_tech.php
@@ -1,3 +1,16 @@
 <?php
 
-return [];
+return [
+
+    // 异步采用统一的回调地址，通过触发不同的事件来做出相应。
+    'callback' => [
+        'create_account_p' => '',
+    ],
+
+    'success_url' => [
+        'create_account_p' => '',
+
+    ]
+
+
+];
