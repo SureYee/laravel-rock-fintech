0caecb3f5bcc4bf2d32554d66e5b67d4fdbaf1af
yixu <yixu@sureyee.com>
2018-08-31 10:52:15 +0800

    添加request_log和response_log数据表

--- src/database/migrations/2018_08_20_000000_create_rft_request_logs_table.php
+++ src/database/migrations/2018_08_20_000000_create_rft_request_logs_table.php
@@ -10,13 +10,11 @@ class CreateRftRequestLogsTable extends Migration
     {
         Schema::create('rft_request_logs', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('batch_no')->comment('批次请求的批次号');
-            $table->string('serial_no')->comment('交易流水号或订单号');
+            $table->string('batch_no')->nullable()->comment('批次请求的批次号');
+            $table->string('serial_no')->nullable()->comment('交易流水号或订单号');
             $table->string('service')->comment('服务名称');
             $table->string('uuid')->comemnt('请求UUID');
             $table->string('client');
-            $table->string('encode');
-            $table->string('sign_tye');
             $table->string('version');
             $table->string('custom');
             $table->dateTime('request_time');
---
+++ src/database/migrations/2018_08_20_000000_create_rft_response_logs_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRftResponseLogsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('rft_response_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('type', ['async', 'sync'])->comment('回调类型');
+            $table->string('code');
+            $table->string('msg');
+            $table->string('service')->comment('服务名称');
+            $table->string('uuid');
+            $table->string('version');
+            $table->string('sequence_id');
+            $table->string('custom');
+            $table->string('client');
+            $table->dateTime('response_time');
+            $table->text('response_data');
+            $table->timestamps();
+        });
+    }
+}
